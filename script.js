7bc6eef77435d2ebff8f5babb082fa33f156f451
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-09-03 12:34:16 +0700

    [new]

---
+++ script.js
@@ -0,0 +1,11 @@
+function showPopups() {
+  document.getElementById("btnclose").style.display = "block";
+  document.getElementById("popup1").style.display = "block";
+  document.getElementById("popup2").style.display = "block";
+}
+
+function closePopups() {
+  document.getElementById("btnclose").style.display = "none";
+  document.getElementById("popup1").style.display = "none";
+  document.getElementById("popup2").style.display = "none";
+}
